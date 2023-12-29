668741db8e8ab1edd33911b9073694190954b618
Danil Murashkin <danil.murashkin@gmail.com>
2023-12-29 07:09:30 +0600

    Simple read file

--- app/src/main/java/com/danil_murashkin/serial_terminal/MainActivity.kt
+++ app/src/main/java/com/danil_murashkin/serial_terminal/MainActivity.kt
@@ -65,125 +65,23 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
     private fun openFileButtonClick()
     {
         binding.sampleText.text = "Button click"
 
-        //openFileIntentActivity.launch( "audio/wav" )
-        //openFileActivity.launch( "text/plain" )
-        //openFileActivity.launch( arrayOf("text/plain") )
-        resultContracts.launch( arrayOf("text/plain") )
+        openFileActivity.launch( arrayOf("*/*") ) // "text/plain" "audio/wav"
     }
-    private var uri: Uri? = null
-    private val TAG = "MainActivity"
-    private val resultContracts =
-        registerForActivityResult(ActivityResultContracts.OpenDocument()) { u ->
-            if (u == null) {
-                Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show()
-                return@registerForActivityResult
-            }
-            uri = u
-            contentResolver.openInputStream(u)?.bufferedReader()?.use {
-                binding.textView.setText(it.readText())
-            }
-
-            contentResolver.query(u, null, null, null, null)?.let { cursor ->
-                cursor.moveToFirst()
-
-                val fn = cursor.getStringOrNull(
-                    cursor.getColumnIndex(
-                        android.provider.OpenableColumns.DISPLAY_NAME
-                    )
-                )
-                val did = DocumentsContract.getDocumentId(u)
-
-                val doc_id =
-                    cursor.getStringOrNull(cursor.getColumnIndex(DocumentsContract.Document.COLUMN_DOCUMENT_ID))
-                val mime_type =
-                    cursor.getStringOrNull(cursor.getColumnIndex(DocumentsContract.Document.COLUMN_MIME_TYPE))
-                val l_modified =
-                    cursor.getLongOrNull(cursor.getColumnIndex(DocumentsContract.Document.COLUMN_LAST_MODIFIED))
-                val sz =
-                    cursor.getLongOrNull(cursor.getColumnIndex(android.provider.OpenableColumns.SIZE))
-                val fl =
-                    cursor.getIntOrNull(cursor.getColumnIndex(DocumentsContract.Document.COLUMN_FLAGS))
-
-                //if (fl != null) writeBtn((fl and DocumentsContract.Document.FLAG_SUPPORTS_WRITE) != 0)
-
-
-                Log.i(TAG, "---- column: ${cursor.columnNames}")
-
-                Log.i(TAG, "getDocumentId: $did")
-                Log.i(
-                    TAG,
-                    "uri: $u\ndocument_id: $doc_id\nmime_tipe: $mime_type\nlast modified: $l_modified\nsize: $sz\nflags: $fl"
-                )
-
-                for (i: Int in 0 until cursor.columnCount) {
-                    Log.i(TAG, cursor.getColumnName(i) + " : " + cursor.getType(i) + " : ")
-                }
-                cursor.close()
-
-                when (val p = checkUriPermission(
-                    uri,
-                    Binder.getCallingPid(),
-                    Binder.getCallingUid(),
-                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-                )) {
-                    PackageManager.PERMISSION_GRANTED -> {
-                        Toast.makeText(
-                            this,
-                            "Write Permission Granted to: $fn\n(size: $sz)\n",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    PackageManager.PERMISSION_DENIED ->
-                        Toast.makeText(
-                            this,
-                            "Write Permission Denied!!!! filename: $fn\n(size: $sz)",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    else ->
-                        Toast.makeText(
-                            this,
-                            "Down Know what is this permission: $p\nfilename: $fn (size: $sz)",
-                            Toast.LENGTH_LONG
-                        ).show()
-                }
-
-                title = "$fn opened"
-
-                val cursor2 = contentResolver.query(
-                    u,
-                    arrayOf(DocumentsContract.Document.COLUMN_FLAGS),
-                    null,
-                    null,
-                    null
-                )
-                val flags = if (cursor2?.moveToFirst() == true && !cursor2.isNull(0)) {
-                    cursor2.getInt(0)
-                } else {
-                    0
-                }
-
-                Log.i(
-                    TAG,
-                    "-------- flags: $flags --- fl: $fl -------- isWritable: ${(flags and DocumentsContract.Document.FLAG_SUPPORTS_WRITE) != 0} --------------"
-                )
-            }
-        }
-
-    private val openFileActivity = registerForActivityResult( ActivityResultContracts.OpenDocument() ) // GetContent
+    private val openFileActivity = registerForActivityResult( ActivityResultContracts.OpenDocument() )
     { uri ->
         if (uri != null) {
-            binding.sampleText.text = uri.path
-            val fileRead:File = File(uri.path)
-            val stream: FileInputStream = openFileInput(uri.path)
-            //val data = ByteArray(1000)
-            //stream.read(data)
-            //stream.close()
+            contentResolver.openInputStream(uri)?.bufferedReader()?.use {
+                binding.textView.setText( it.readText() )
+            }
         }
     }
+
+
 
     companion object {
         // Used to load the 'serial_terminal' library on application startup.
